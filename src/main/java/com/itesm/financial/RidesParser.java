6554c554fa6df0baf1a78f9eae1f92aa4c8c72d1
jeasika <jesika_18_@hotmail.com>
2022-05-12 01:45:46 -0500

    first commit

---
+++ src/main/java/com/itesm/financial/RidesParser.java
@@ -0,0 +1,36 @@
+package com.itesm.financial;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class RidesParser {
+
+  static final String DATE_FORMAT = "yyyy-MM-dd hh:mm:ss";
+
+  public static Ride parseFromList(List<String> line) {
+
+    Ride newRide = null;
+    try {
+      long taxiId = Long.parseLong(line.get(0));
+      Date pickUpTime = new SimpleDateFormat(DATE_FORMAT).parse(line.get(1));
+      Date dropOffTime = new SimpleDateFormat(DATE_FORMAT).parse(line.get(2));
+      int passengerCount = Integer.parseInt(line.get(3));
+      double tripDistance = Double.parseDouble(line.get(4));
+      double totalAmount = Double.parseDouble(line.get(5));
+
+      newRide =
+          new Ride(
+              taxiId,
+              pickUpTime,
+              dropOffTime,
+              passengerCount,
+              tripDistance,
+              totalAmount);
+
+    } catch (Exception e) { }
+
+    return newRide;
+  }
+
+}
---
+++ src/main/java/com/itesm/financial/WebReport.java
@@ -0,0 +1,66 @@
+package com.itesm.financial;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class WebReport {
+
+    public String createContent(List<Ride> rides) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(createHeaders("Taxi Report"));
+        builder.append(createTableHeaders());
+        rides.forEach( ride -> {
+            builder.append(addRide(ride));
+        });
+        builder.append(closeTableHeaders());
+
+        return builder.toString();
+    }
+
+    public void createFile(String content) throws IOException {
+        FileWriter fileWriter = new FileWriter("financial-report.html");
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        printWriter.print(content);
+        printWriter.close();
+    }
+
+    private String createHeaders(String title){
+        return "<h1>" + title + "</h1>";
+    }
+
+    private String createTableHeaders() {
+        return "<table>" +
+                "<tr>" +
+                    "<th> TaxiID </th>" +
+                    "<th> Pickup time </th>" +
+                    "<th> Dropoff time </th>" +
+                    "<th> Passenger count </th>" +
+                    "<th> Trip Distance </th>" +
+                    "<th> Total amount </th>" +
+                "</tr>";
+    }
+
+    private String closeTableHeaders() {
+        return "</table>";
+    }
+
+    private String addRide(Ride ride) {
+        return "<tr>" +
+                "<td>" + ride.getTaxiId() + "</td>" +
+                "<td>" + ride.getPickUpTime() + "</td>" +
+                "<td>" + ride.getDropOffTime() + "</td>" +
+                "<td>" + ride.getPassengerCount() + "</td>" +
+                "<td>" + ride.getTripDistance() + "</td>" +
+                "<td>" + formatAmount(ride.getTollsAmount()) + "</td>" +
+                "</tr>";
+    }
+
+    private String formatAmount(double amount) {
+        if(amount < 0) {
+            return "<span style='color:red'>" + amount + "</span>";
+        }
+        return Double.toString(amount);
+    }
+}
